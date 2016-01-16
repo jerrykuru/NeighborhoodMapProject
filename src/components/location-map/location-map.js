6b876c8ce70d4d29a49b1c8daf318c9f6189677a
Jerry Kuru <jerrykuru@gmail.com>
2016-01-16 12:57:49 -0500

    Added Google Maps Loading

--- src/components/location-map/location-map.js
+++ src/components/location-map/location-map.js
@@ -1,9 +1,23 @@
 import ko from 'knockout';
 import locationMapTemplate from 'text!./location-map.html';
+import google from 'google-map-api';
 
 class LocationMapViewModel {
 	constructor() {
+		// console.log("locationMapTemplate", locationMapTemplate);
+		// console.log("map_canvas", $('#map_canvas')[0]);
+		// console.log("googe", window.google);
+		// console.log("document", document);
+		// console.log("window.google.maps", window.google.maps);
+		 var map = window.google.maps.Map(document.getElementById('map_canvas'), {
+			zoom: 5,
+			center: new window.google.maps.LatLng(55, 11),
+			mapTypeId: window.google.maps.MapTypeId.ROADMAP
+		});
+		 console.log("map", map);
 	}
+
+
 
 }
 
